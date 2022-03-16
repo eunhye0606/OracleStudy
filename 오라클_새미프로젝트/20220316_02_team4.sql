e33b2eeafb64a6ffb098241de066ed3368791f47
eunhye <cherry5813@naver.com>
2022-03-16 18:01:21 +0900

    오라클 새미 프로젝트 진행중

---
+++ 오라클_새미프로젝트/20220316_02_team4.sql
@@ -0,0 +1,15 @@
+SELECT USER
+FROM DUAL;
+--==>>TEAM4
+
+--------------------------------------------------------------------------------
+/*
+CREATE TABLE STUDENT_REGISTER
+(STUDENT_CODE    VARCHAR2(10)
+,ID              VARCHAR2(20)
+,PASSWORD        VARCHAR2(30) 
+,NAME            VARCHAR2(20)
+,SSN             VARCHAR2(13)
+,REGISTER_DATE   DATE            
+);
+*/
